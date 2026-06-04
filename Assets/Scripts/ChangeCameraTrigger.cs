730775d044f4d4117051e7f4a55f67dfd7fa93c2
Grimmeld <lea.parnaudeau@live.fr>
2026-06-04 10:38:04 +0200

    Modif Blockout f

--- Assets/Scripts/ChangeCameraTrigger.cs
+++ Assets/Scripts/ChangeCameraTrigger.cs
@@ -3,13 +3,22 @@ using UnityEngine;
 public class ChangeCameraTrigger : MonoBehaviour
 {
     [SerializeField] public Camera mainCamera;
-    public float distanceCamera;
+    public float distanceCamera = 15f;
+    public float pitchCamera = 45f;
+    public float yawCamera = 40f;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
             mainCamera.GetComponent<IsometricCameraFollow>().distance = distanceCamera;
+            mainCamera.GetComponent<IsometricCameraFollow>().pitch = pitchCamera;
+            mainCamera.GetComponent<IsometricCameraFollow>().yaw = yawCamera;
 
         }
     }
