9b8f878abf23d1416c90ca66972dc0432bf9a837
Samuel_AD <Skide.98@gmail.com>
2026-06-08 14:14:48 +0200

    Add Main_menu / Button & Load all scene

--- Assets/Scripts/UI/UI_Script/MainMenu_Event.cs
+++ Assets/Scripts/UI/UI_Script/MainMenu_Event.cs
@@ -1,16 +1,60 @@
+using Mono.Cecil.Cil;
+using Unity.VectorGraphics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class MainMenu_Event : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private UIDocument UIMain_Menu;
+    private Button ButtonStart;
+    private Button ButtonZoo;
+    private Button ButtonCredit;
+    private Button ButtonQuit;
+    private void Awake()
     {
-        
+        UIMain_Menu = GetComponent<UIDocument>();
+        //Start button to load Main_Scene
+        ButtonStart = UIMain_Menu.rootVisualElement.Q("B_Start") as Button;
+        ButtonStart.RegisterCallback<ClickEvent>(OnPlayGameClick);
+        //Button to Zoo Scene
+        ButtonZoo = UIMain_Menu.rootVisualElement.Q("B_Zoo") as Button;
+        ButtonZoo.RegisterCallback<ClickEvent>(OnPlayZooClick);
+        //Button to credit of the team membres
+        ButtonCredit = UIMain_Menu.rootVisualElement.Q("B_Credit") as Button;
+        ButtonCredit.RegisterCallback<ClickEvent>(OnPlayCreditClick);
+        //Button to quit game
+        ButtonQuit = UIMain_Menu.rootVisualElement.Q("B_Quit") as Button;
+        ButtonQuit.RegisterCallback<ClickEvent>(QuitClick);
+    }
+    private void OnPlayGameClick(ClickEvent evt)
+    {
+        Debug.Log("Bienvenue a XAR SAROTH !!!");
+        SceneManager.LoadScene("BlockoutF GA");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnPlayZooClick(ClickEvent evt)
     {
-        
+        Debug.Log(" LES ZOO ZOO");
+        SceneManager.LoadScene("Scene_Zoo");
     }
+    private void OnPlayCreditClick(ClickEvent evt)
+    {
+        Debug.Log("The End M*therfucker");
+        SceneManager.LoadScene("04_CreditMenu");
+    }
+    private void QuitClick(ClickEvent evt)
+    {
+        Debug.Log("Tmort Tmort AAAAAAH");
+        Application.Quit();
+    }
+
+
+    private void OnDisable()
+    {
+        ButtonStart.UnregisterCallback<ClickEvent>(OnPlayGameClick);
+        ButtonZoo.UnregisterCallback<ClickEvent>(OnPlayZooClick);
+        ButtonCredit.UnregisterCallback<ClickEvent>(OnPlayCreditClick);
+    }
+
 }
