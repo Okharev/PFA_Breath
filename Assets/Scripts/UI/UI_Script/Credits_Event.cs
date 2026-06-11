3f6795f5ddcf5696241fd278fd5fbef913cfd582
Samuel_AD <Skide.98@gmail.com>
2026-06-11 10:58:57 +0200

    Add ui Credit script & fix UI

--- Assets/Scripts/UI/UI_Script/Credits_Event.cs
+++ Assets/Scripts/UI/UI_Script/Credits_Event.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class Credits_Event : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private Button ButtonBack;
+    private UIDocument UI_Credits;
+    private AudioManager audioManager;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        UI_Credits = GetComponent<UIDocument>();
+        //Start button to load Main_Scene
+        ButtonBack = UI_Credits.rootVisualElement.Q("B_Back") as Button;
+        ButtonBack.RegisterCallback<ClickEvent>(BackOnMenu);
+    }
+    private void OnDisable()
+    {
+        ButtonBack.UnregisterCallback<ClickEvent>(BackOnMenu);
+    }
+    private void BackOnMenu(ClickEvent evt)
+    {
+        SceneManager.LoadScene("00_StartMainMenu");
     }
 }
