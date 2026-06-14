f0bc308182e5075a50150c290f1ddd35d10560a9
Grimmeld <lea.parnaudeau@live.fr>
2026-06-14 19:22:07 +0200

    Add end level interaction + modif 01_level
    Add game over script to the level
    Add animation to project

---
+++ Assets/Scripts/TriggerEndDemo.cs
@@ -0,0 +1,64 @@
+using Ability.NewAbilitySystem;
+using Dialogues.UI;
+using Skills.UI;
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TriggerEndDemo : MonoBehaviour, IInteractable
+{
+    public static TriggerEndDemo Instance { get; private set; }
+
+    [SerializeField] private EndDemo_Event enddemo;
+    [SerializeField] private DialogueDebugDirectTrigger dialogueTrigger;
+
+    public bool isEntered = false;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        dialogueTrigger.onCleared += activateEndDemoTrigger;
+        // Deactivate the end level at the start of the level
+        gameObject.SetActive(false);
+    }
+
+    public void activateEndDemoTrigger(bool state)
+    {
+        gameObject.SetActive(state);
+    }
+
+    public void EndDemo()
+    {
+        if (enddemo == null) 
+        {     
+            Debug.Log("UI End Demo missing");
+            return;
+        }
+        enddemo.gameObject.SetActive(true);
+    }
+
+
+    public void Interact(GameObject instigator)
+    {
+        // Interactable when close enough
+        StartCoroutine(PlaySound());
+    }
+
+    private IEnumerator PlaySound()
+    {
+        AudioSource s = GetComponent<AudioSource>();
+        s.Play();
+
+
+        yield return new WaitForSeconds(s.clip.length);
+        EndDemo();
+
+    }
+
+}
